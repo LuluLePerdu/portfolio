6c592a11aeac75f9a9b5a8d526a4f856456c829a
Ludwi <ludwig-emmanuel@hotmail.com>
2025-07-24 23:39:08 -0400

    feat: replace anchor tags with Next.js Link component in SideNav

--- aegis/src/components/shell/sidenav/SideNav.tsx
+++ aegis/src/components/shell/sidenav/SideNav.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 export function SideNav() {
   return (
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
