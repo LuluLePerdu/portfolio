93a3df4fea9020d573b216859ffed908c67b573f
Ludwi <ludwig-emmanuel@hotmail.com>
2025-07-24 23:51:06 -0400

    test deploy

--- aegis/src/app/page.tsx
+++ aegis/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <h1>Welcome to Aegis</h1>
       <p>
-        Aegis is a Next.js portfolio  application showcasing my work as a Full-Stack Developer, DevOps Engineer, and Cloud Enthusiast.
+        Aegis is a Next.js portfolio  application showcasing my work as a Full-Stack Developer, DevOps Engineer, and Cloud Enthusiast..
       </p>
     </main>
   );
