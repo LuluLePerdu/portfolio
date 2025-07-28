b39b40500882a50b60d014abe916b088d41c0abf
Ludwi <ludwig-emmanuel@hotmail.com>
2025-07-28 00:54:20 -0400

    fix: Update sitemap URLs to reflect the correct domain

--- aegis/src/app/sitemap.ts
+++ aegis/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://ludwig-dufour.dev'
+  const baseUrl = 'https://ludwig-emmanuel.dev'
   
   return [
     {
